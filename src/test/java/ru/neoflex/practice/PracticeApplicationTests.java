cd463a296760cc0a801a17ac85eed7f056d7662a
Hacker2999 <wwwhuker.mafka@gmail.com>
2023-06-21 01:49:25 +0300

    Update PracticeApplicationTests.java

--- src/test/java/ru/neoflex/practice/PracticeApplicationTests.java
+++ src/test/java/ru/neoflex/practice/PracticeApplicationTests.java
@@ -1,13 +1,34 @@
 package ru.neoflex.practice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PracticeApplicationTests {
-
+	@Autowired
+	private MockMvc mockMvc;
 	@Test
-	void contextLoads() {
+	public void shouldReturnNumberPlus() throws Exception {
+		this.mockMvc
+				.perform(get("/plus/5/5")) // perform a request that can be chained
+				.andDo(print()) // we log the result
+				.andExpect(content().string(containsString("10"))); // we check that the Body of the answer contains our expectation
+	}
+	@Test
+	public void shouldReturnNumberMinus() throws Exception {
+		this.mockMvc
+				.perform(get("/minus/8/7")) // perform a request that can be chained
+				.andDo(print()) // we log the result
+				.andExpect(content().string(containsString("1"))); // we check that the Body of the answer contains our expectation
 	}
 
 }
